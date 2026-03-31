bff7f570c14e04124cbe88843c3d4c822fbbf306
noah <noahvf16@outlook.com>
2026-03-30 21:40:42 -0300

    feat: add Password value object with validation rules

---
+++ AuthContext/Context/Auth/ValueObject/Password.cs
@@ -0,0 +1,21 @@
+﻿namespace AuthContext.Context.Auth.ValueObject;
+
+public class Password
+{
+    public String Value { get; private set; }
+
+    public Password(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(value))
+            throw new Exception("Password is required");
+
+        if (value.Length > 16)
+            throw new Exception("Password is too long");
+
+        if (value.Length < 8)
+            throw new Exception("Password is too short");
+        
+        this.Value = value;
+        
+    }
+}
