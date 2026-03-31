fe8ba30ec18f09bb9d4eceea7f20688b64cceee7
noah <noahvf16@outlook.com>
2026-03-30 21:45:22 -0300

    feat: add Email and Password properties to User class with constructor

--- AuthContext/Context/Auth/Entities/User.cs
+++ AuthContext/Context/Auth/Entities/User.cs
@@ -1,6 +1,18 @@
-﻿namespace AuthContext.Context.Auth.Entities;
+﻿using AuthContext.Context.Auth.ValueObject;
+
+namespace AuthContext.Context.Auth.Entities;
 
 public class User
 {
+    public Email Email { get; private set; }
+
+    public Password Password { get; private set; }
+
+    public User(Email email, Password password)
+    {
+        Email = email;
+        Password = password;
+    }
+    
     
 }
