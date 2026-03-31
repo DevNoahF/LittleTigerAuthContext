9e0401ba2b2dd53949ee85f70cdad5daec1533dd
noah <noahvf16@outlook.com>
2026-03-30 21:38:01 -0300

    feat: add Email value object and update User class with email and password properties

---
+++ AuthContext/Context/Auth/ValueObject/Email.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthContext.Context.Auth.ValueObject;
+
+public class Email
+{
+    public String Value { get; private set; }
+    
+    public  Email(String value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(value))
+            throw new Exception("Email is required");
+        if (value.Contains("@") &&  value.Contains("."))
+            this.Value = value;
+    }
+    
+   
+    
+}
